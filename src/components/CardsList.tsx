ff2a27c39b3dd07c81479788a8f27eef9d93e885
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-12-28 16:08:16 +0300

    feat: add not authorized message

--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react';
 import { postsContext } from '../context/postsContext';
+import { tokenContext } from '../context/tokenContext';
 import { Card } from './Card/Card';
+import { NotAuthorized } from './UI/NotAuthorized';
 import { SkeletonPost } from './UI/SkeletonPost';
 
 export function CardsList() {
   const posts = useContext(postsContext);
-  return (
+  const token = useContext(tokenContext);
+  return token ? (
     <ul className="cardsList">
       {posts.length
         ? posts.map(({ author, title, previewImage, id }) => (
@@ -15,5 +18,7 @@ export function CardsList() {
             .fill(0)
             .map((_, i) => <SkeletonPost key={i} />)}
     </ul>
+  ) : (
+    <NotAuthorized />
   );
 }
